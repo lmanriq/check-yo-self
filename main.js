2989c1727fd193413f2dc1dc488272b5a0b2cdee
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-09 17:27:33 -0700

    Write addTaskItem function

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ plusButton.addEventListener('click', addTaskItem)
 taskItemBox.addEventListener('click', deleteTaskItem)
 
 function addTaskItem() {
-
+  var taskItemHTML = `<div class="item">
+    <img src="assets/delete.svg" alt="delete icon"><p>${taskItemInput.value}</p>
+  </div>`
+  taskItemBox.insertAdjacentHTML('beforeend', taskItemHTML);
+  taskItemInput.value = '';
 }
 
 function deleteTaskItem() {
-  
+
 }
