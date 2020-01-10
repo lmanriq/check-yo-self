25ad4f3eeb8da6a02df9e072e969c04cbf25dfb5
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-09 17:23:37 -0700

    Outline taskItem functions

---
+++ main.js
@@ -0,0 +1,14 @@
+var taskItemBox = document.querySelector('.task-item-box');
+var taskItemInput = document.getElementById('task-item-input');
+var plusButton = document.getElementById('add-task-btn');
+
+plusButton.addEventListener('click', addTaskItem)
+taskItemBox.addEventListener('click', deleteTaskItem)
+
+function addTaskItem() {
+
+}
+
+function deleteTaskItem() {
+  
+}
