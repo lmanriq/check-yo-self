189c6348d09b2399584a6d1878782917a7c3748c
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-10 19:29:05 -0700

    Fix checkbox functionality error

--- main.js
+++ main.js
@@ -13,15 +13,17 @@ tasksListsSection.addEventListener('click', function() {
   changeCheckedStatus();
   addTaskListsToStorage();
   deleteTaskCard(event);
+  checkIfChecked();
+  // addTasksOnLoad();
 })
 
 function checkIfChecked() {
   var allCheckBoxes = document.querySelectorAll('input[type="checkbox"]');
-  allCheckBoxes.forEach(function(checkBox) {
-    if (checkBox.checked == true) {
-      checkBox.disabled = true;
+  for (var i = 0; i < allCheckBoxes.length; i++) {
+    if (allCheckBoxes[i].checked) {
+      allCheckBoxes[i].disabled = true;
     }
-  })
+  }
 }
 
 function addTaskListsToStorage() {
@@ -32,17 +34,32 @@ function changeCheckedStatus() {
   if (event.target.classList.contains('checkbox')) {
     console.log(event.target.id);
     // var newTasksList;
-    taskLists.forEach(function(taskList) {
-      taskList.tasks.forEach(function(task) {
-        if (event.target.id === task.id) {
-          task.completed = true;
-        }
+    // for (var i = 0; i < taskLists.length; i++) {
+    //   for (var j = 0; j < taskLists[i].tasks.length; j++) {
+    //     taskLists[i].tasks[j].updateTask(event.target.id, taskLists[i].tasks[j].content, taskLists[i].tasks[j].completed)
+    //   }
+    // }
 
-      })
+    for (var i = 0; i < taskLists.length; i++) {
+      for (var j = 0; j < taskLists[i].tasks.length; j++) {
+        if (event.target.id === taskLists[i].tasks[j].id) {
+          console.log('howdy')
+          taskLists[i].updateTask(event.target.id);
+          event.target.disabled = true;
+        }
+      }
+    }
+    // taskLists.forEach(function(taskList) {
+    //   taskList.tasks.forEach(function(task) {
+    //     if (event.target.id === task.id) {
+    //       task.completed = true;
+    //     }
+    //
+    //   })
       //The checked status is working, but if I use the code below, I get duplicates
       //of all of my cards
       // taskList.saveToStorage();
-    })
+    // })
     // taskLists = newTasksList;
     //taskLists.push(this);
     // window.localStorage.setItem('task lists', JSON.stringify(taskLists));
@@ -99,11 +116,33 @@ function addTaskCard(id) {
       <input class="checkbox" type="checkbox"><p>${task.innerText}</p>
     </div>`;
   });
+
+  // var checklistHTML = '';
+  // for (var i = 0; i < allTaskItems.length; i++) {
+  //   var taskItems = allTaskItems[i].tasks;
+  //   for (var j = 0; j < taskItems.length; j++) {
+  //     var taskId = taskItems[j].id;
+  //     taskItems[j] = new Task(taskId, taskItems[j].content, taskItems[j].completed);
+  //     var checkedStatus = '';
+  //     if (taskItems[j].completed === true) {
+  //       checkedStatus = `checked="checked"`
+  //     }
+  //     checklistHTML += `<div class="check-pair">
+  //       <input id=${taskId} class="checkbox" type="checkbox" ${checkedStatus}><p>${taskItems[j].content}</p>
+  //     </div>`;
+  //   }
+  //   var taskCard = makeTaskCard(taskLists[i].id, taskLists[i].title, checklistHTML);
+  //   tasksListsSection.insertAdjacentHTML('afterbegin', taskCard);
+  //   checkIfChecked();
+  // }
+
   var taskCard = makeTaskCard(id, taskTitleInput.value, checklistHTML);
   tasksListsSection.insertAdjacentHTML('afterbegin', taskCard);
 }
 
+checkStorage();
 addTasksOnLoad();
+
 
 function makeTaskCard(id, title, checklistHTML) {
   var taskCard = `<div id=${id} class="task-card">
@@ -125,7 +164,7 @@ function makeTaskCard(id, title, checklistHTML) {
   return taskCard;
 }
 
-function addTasksOnLoad() {
+function checkStorage() {
   if (localStorage.getItem('task lists')) {
     noTasksMsg.remove();
     taskLists = JSON.parse(localStorage.getItem('task lists'));
@@ -133,6 +172,21 @@ function addTasksOnLoad() {
       var listId = taskLists[g].id;
       var listTasks = taskLists[g].tasks;
       taskLists[g] = new ToDoList(listId, taskLists[g].title, listTasks);
+    }
+  }
+}
+
+function addTasksOnLoad() {
+  // if (localStorage.getItem('task lists')) {
+  //   noTasksMsg.remove();
+  //   taskLists = JSON.parse(localStorage.getItem('task lists'));
+  //   for (var g = 0; g < taskLists.length; g++) {
+  //     var listId = taskLists[g].id;
+  //     var listTasks = taskLists[g].tasks;
+  //     taskLists[g] = new ToDoList(listId, taskLists[g].title, listTasks);
+  //   }
+    if (localStorage.getItem('task lists')) {
+      tasksListsSection.innerHTML = ''
     }
     for (var i = 0; i < taskLists.length; i++) {
       var checklistHTML = '';
@@ -152,7 +206,7 @@ function addTasksOnLoad() {
       tasksListsSection.insertAdjacentHTML('afterbegin', taskCard);
       checkIfChecked();
     }
-  }
+  // }
 }
 
 function addTasksToStorage() {
@@ -166,7 +220,8 @@ function addTasksToStorage() {
   })
   var toDo = new ToDoList(id, taskTitleInput.value, taskItems);
   toDo.saveToStorage();
-  addTaskCard(id);
+  // addTaskCard(id);
+  addTasksOnLoad();
 }
 
 function activatePlusBtn() {
